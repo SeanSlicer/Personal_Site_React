bee797024d74c94708035fef498c8a7c2e29df94
Sean Slicer <seanslicer@gmail.com>
2021-10-13 05:13:11 -0500

    Initial_Site_Commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import "./App.css";
 import Header from "./Header";
 import React, { useState } from "react";
 import Profile from "./Components/Profile/Profile";
-import Projects from "./Components/Projects/Projects";
 
 function App() {
-  const [showContent, setShowContent] = useState("Profile");
   return (
     <div>
       <header className="site-container App">
-        <Header setShowContent={setShowContent} />
+        <Header />
       </header>
       <div className="background-fix profileCard">
-        {showContent === "Profile" && <Profile />}
-        {showContent === "Projects" && <Projects />}
+        <Profile />
       </div>
     </div>
   );
 }
-
 export default App;
