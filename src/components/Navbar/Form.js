425c5c2808cd4967470bf81e8e609cd6edab900e
Sean Slicer <87379816+SeanSlicer@users.noreply.github.com>
2022-01-11 01:13:38 -0600

    cmmt again

--- src/components/Navbar/Form.js
+++ src/components/Navbar/Form.js
@@ -5,17 +5,22 @@ export default function Form() {
   return (
     <form
       name="contactform"
-      method="POST"
-      action="contact/?success=true"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
+      // method="POST"
+      // action="contact/?success=true"
+      // data-netlify="true"
+      // data-netlify-honeypot="bot-field"
       className={styles.contactForm}
     >
       {/* <label htmlFor="name">Name: </label> */}
       <input type="text" name="name" id="name" placeholder="Enter your name" />
 
       {/* <label htmlFor="email">Email: </label> */}
-      <input type="email" name="email" placeholder="Enter your email" />
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Enter your email"
+      />
 
       {/* <label htmlFor="message">Mssg: </label> */}
       <textarea
@@ -25,7 +30,7 @@ export default function Form() {
         rows="10"
         placeholder="I'd like to get in touch about..."
       />
-      <div data-netlify-recaptcha="true"></div>
+      {/* <div data-netlify-recaptcha="true"></div> */}
 
       <br />
 
