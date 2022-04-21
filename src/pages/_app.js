bf38c2300070eb65d4f7c5097c993d603bba19e2
Sean Slicer <87379816+SeanSlicer@users.noreply.github.com>
2022-04-21 12:09:37 -0500

    Update Commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import { Navbar } from "../components/Navbar";
 import styles from "../styles/page.module.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className={styles.wrapper}>
+      <Head>
+        <link rel="shortcut icon" href="" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </div>
